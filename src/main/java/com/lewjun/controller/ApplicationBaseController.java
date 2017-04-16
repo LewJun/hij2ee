0e0d688af5c92451871ba323d876031fd949679f
lewjun <lewjun072@sina.com>
2017-04-16 22:14:04 +0800

    用于其它Controller继承，使用公共属性

---
+++ src/main/java/com/lewjun/controller/ApplicationBaseController.java
@@ -0,0 +1,13 @@
+package com.lewjun.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author LewJun
+ */
+public class ApplicationBaseController {
+	protected static final Logger LOGGER = LoggerFactory.getLogger(ApplicationBaseController.class);
+
+}
